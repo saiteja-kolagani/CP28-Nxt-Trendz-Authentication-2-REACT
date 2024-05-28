eacd52f326a2fd720b101fc16c0d2cc927f5b972
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-05-28 12:01:59 +0000

    No Error

---
+++ src/components/Cart/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="container-cart">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="image-cart"
+      />
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="container-products">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="image-products"
+      />
+    </div>
+  </>
+)
+
+export default Products
